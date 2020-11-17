1da40f5d8e46b3d2c49a385460ac094c497a6e62
Hunter Willingham <hwilling@ycp.edu>
2020-11-17 16:51:23 -0500

    Revert "Revert "Initial commit""
    This reverts commit 0bd8dee70171e0aee48a3d728be41da4a9cad81c.

---
+++ color_bot/Controllers/LinkCreator.cs
@@ -0,0 +1,17 @@
+﻿namespace color_bot.Controllers
+{
+    internal class LinkCreator
+    {
+        private readonly string _hexWebLink = @"https://www.colorhexa.com/";
+
+        public LinkCreator() { }
+
+        /// <summary>
+        /// Creates a link to a hex color value based on the hex input.
+        /// Currently usees https://www.colorhexa.com/
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns>web address to desired hex color</returns>
+        public string CreateLink(string hex) => _hexWebLink + hex;
+    }
+}
